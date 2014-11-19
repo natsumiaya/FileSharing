68318220eff38a2a536268f8a33091135fd3acb3
Agus Adi Wirawan <agus4171@users.noreply.github.com>
2014-11-20 00:32:25 +0700

    Update ThreadClient.java

--- ThreadClient.java
+++ ThreadClient.java
@@ -3,96 +3,105 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package filesharing;
+package serverfile;
 
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.net.SocketAddress;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.io.Serializable;
+import objectfile.ObjectFile;
 
 /**
  *
- * @author aya
+ * @author MAgus
  */
-public class ThreadClient {
-private Socket sockClient;
-private ArrayList<ThreadClient> alThread;
-private BufferedReader br = null;
-private BufferedOutputStream bos = null;
-private DataInputStream dis = null;
-private SocketAddress sa = null;
+public class ThreadClient implements Runnable {
+    private Socket socketClient;
+    private ArrayList<ThreadClient> alThread;
+    private BufferedReader br = null;
+    private BufferedOutputStream bos = null;
+    private DataInputStream dis = null;
+    private SocketAddress sa = null;
+    public int BUFFER_SIZE = 1024;   
+    private ObjectOutputStream ous;
+    private ObjectInputStream ois;
+    private String nama;
+    
+    public ThreadClient(Socket socketClient, ArrayList<ThreadClient> alThread)
+    {
+        this.socketClient = socketClient;
+        this.alThread = alThread;
+        this.sa = socketClient.getRemoteSocketAddress();
+    }
+    
+    @Override
+    public void run()
+    {
+        try {
+            ous  = new ObjectOutputStream(socketClient.getOutputStream());
+            ois = new ObjectInputStream(socketClient.getInputStream());
+            ObjectFile of = new ObjectFile();
+            try {
+                while((of = (ObjectFile) ois.readObject()) != null)
+                {
+                    if(of.getCommand().equals("konek"))
+                    {
+                        this.nama = of.getUser();
+                    }
+                    for (int i = 0; i < this.alThread.size(); i++) {
+                        System.out.println("nama : "+ alThread.get(i).nama);
+                    }
+                }
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(ThreadClient.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            getSockClient().close();
+            synchronized(this.alThread)
+            {
+                this.alThread.remove(this);
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(ThreadClient.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void send(String msg) throws IOException
+    {
+        this.bos.write(msg.getBytes());
+        this.bos.flush();
+    }
+    
+    public synchronized void sendMessage(String msg) throws IOException
+    {
+        for(int i=0;i<this.alThread.size();i++)
+        {
+            ThreadClient tc = this.alThread.get(i);
+            tc.send(msg);
+        }
+    }
 
+    /**
+     * @return the sockClient
+     */
+    public Socket getSockClient() {
+        return socketClient;
+    }
 
-public ThreadClient(Socket sockClient, ArrayList<ThreadClient> alThread)
-{
-this.sockClient=sockClient;
-this.alThread=alThread;
-this.sa = sockClient.getRemoteSocketAddress();
+    /**
+     * @param sockClient the sockClient to set
+     */
+    public void setSockClient(Socket sockClient) {
+        this.socketClient = sockClient;
+    }
+    
 }
-
-public void run()
-{
-try {
-bos=new BufferedOutputStream(getSockClient().getOutputStream());
-dis=new DataInputStream(getSockClient().getInputStream());
-bos.write("Selamat datang...\r\n".getBytes());
-bos.flush();
-br = new BufferedReader(new InputStreamReader(dis));
-String msg;
-while((msg = br.readLine()) != null)
-{
-msg=this.sa.toString() + ":" + msg+"\r\n";
-this.sendMessage(msg);
-bos.flush();
-}
-System.out.println("Koneksi terputus");
-bos.close();
-getSockClient().close();
-synchronized(this.alThread)
-{
-this.alThread.remove(this);
-}
-} catch (IOException ex) {
-Logger.getLogger(ThreadClient.class.getName()).log(Level.SEVERE, null, ex);
-}
-}
-
-public void send(String msg) throws IOException
-{
-this.bos.write(msg.getBytes());
-this.bos.flush();
-}
-
-public synchronized void sendMessage(String msg) throws IOException
-{
-for(int i=0;i<this.alThread.size();i++)
-{
-ThreadClient tc = this.alThread.get(i);
-tc.send(msg);
-}
-}
-
-/**
-* @return the sockClient
-*/
-public Socket getSockClient() {
-return sockClient;
-}
-
-/**
-* @param sockClient the sockClient to set
-*/
-public void setSockClient(Socket sockClient) {
-this.sockClient = sockClient;
-}
-//}
 
 /*
 Chat Message Class
