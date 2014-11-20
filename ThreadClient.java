93727601a0bc718c3e41f0bbb0f0c474cc215d32
Agus Adi Wirawan <agus4171@users.noreply.github.com>
2014-11-20 20:20:06 +0700

    Update ThreadClient.java

--- ThreadClient.java
+++ ThreadClient.java
@@ -5,8 +5,6 @@
  */
 package serverfile;
 
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
@@ -28,22 +26,27 @@ import objectfile.ObjectFile;
 public class ThreadClient implements Runnable {
     private Socket socketClient;
     private ArrayList<ThreadClient> allThread;
-    private BufferedReader br = null;
-    private BufferedOutputStream bos = null;
-    private DataInputStream dis = null;
+    //private BufferedReader br = null;
+    //private BufferedOutputStream bos = null;
     private SocketAddress sa = null;   
     private ObjectOutputStream ous;
     private ObjectInputStream ois;
     private String nama;
     private String fnama;
+    private DataInputStream diStream;
     private DataOutputStream doStream;
+    private File fileLoc = null;
     public int BUFFER_SIZE = 1024;
+    private ArrayList<String> lUser;
+    private ArrayList<String> lFile;
     
     public ThreadClient(Socket socketClient, ArrayList<ThreadClient> allThread)
     {
         this.socketClient = socketClient;
         this.allThread = allThread;
         this.sa = socketClient.getRemoteSocketAddress();
+        this.lUser = new ArrayList<>();
+        this.lFile = new ArrayList<>();
     }
     
     @Override
@@ -52,32 +55,44 @@ public class ThreadClient implements Runnable {
         try {
             ous  = new ObjectOutputStream(socketClient.getOutputStream());
             ois = new ObjectInputStream(socketClient.getInputStream());
-            new File("F:/cache").mkdirs();
             ObjectFile objf = new ObjectFile();
             try {
                 while((objf = (ObjectFile) ois.readObject()) != null)
                 {
-                    if(objf.getCommand().equals("konek"))
+                    if(objf.getCommand().equals("connect"))
                     {
                         this.nama = objf.getUser();
-                    }
-                    else if(objf.getCommand().equals("kirim"))
-                    {
-                        System.out.println("tulis");
-                        //ObjectFile objfn = (ObjectFile) ois.readObject();
-                        this.fnama = objf.getNama();
-                        File file = new File("F:/cache/" + objf.getNama());
-                        doStream = new DataOutputStream(new FileOutputStream(file));
-                        doStream.write(objf.getIsi());
-                        System.out.println("file write");
-                    }
-                    for (int i = 0; i < this.allThread.size(); i++) {
-                        System.out.println("nama : "+ allThread.get(i).nama);
-                    }
-                    for (int i = 0; i < this.allThread.size(); i++) {
-                        System.out.println("fnama : "+ allThread.get(i).fnama);
+                        new File("F:/cache/"+this.nama).mkdirs();
+                        System.out.println("User masuk : "+this.nama);
                     }
                     
+                    else if(objf.getCommand().equals("kirim"))
+                    {
+                        System.out.println("Menulis file");
+                        this.fnama = objf.getNama();
+                        File file = new File("F:/cache/" + this.nama +"/"+ objf.getNama());
+                        doStream = new DataOutputStream(new FileOutputStream(file));
+                        doStream.write(objf.getIsi());
+                        System.out.println("File tertulis :"+this.fnama);
+                    }
+                    
+                    else if(objf.getCommand().equals("listUser"))
+                    {
+                        for (int i = 0; i < this.allThread.size(); i++) {
+                            lUser.add(allThread.get(i).nama);
+                            System.out.println("user"+lUser.get(i));
+                        }
+                        send(lUser);
+                    }
+                    
+                    else if(objf.getCommand().equals("listFile"))
+                    {
+                        for (int i = 0; i < this.allThread.size(); i++) {
+                            lFile.add(allThread.get(i).fnama);
+                            System.out.println("user"+lFile.get(i));
+                        }
+                        send(lFile);
+                    }               
                 }
             } catch (ClassNotFoundException ex) {
                 Logger.getLogger(ThreadClient.class.getName()).log(Level.SEVERE, null, ex);
@@ -92,18 +107,19 @@ public class ThreadClient implements Runnable {
         }
     }
     
-    public void send(String msg) throws IOException
+    public void send(Object file) throws IOException
     {
-        this.bos.write(msg.getBytes());
-        this.bos.flush();
+        this.ous.writeObject(file);
+        this.ous.flush();
+        this.ous.reset();
     }
     
-    public synchronized void sendMessage(String msg) throws IOException
+    public synchronized void sendMessage(ObjectFile file) throws IOException
     {
         for(int i=0;i<this.allThread.size();i++)
         {
             ThreadClient tc = this.allThread.get(i);
-            tc.send(msg);
+            tc.send(file);
         }
     }
 
