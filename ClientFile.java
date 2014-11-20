3001c6504833396f78ead9e19feef49ab5ae4e97
Agus Adi Wirawan <agus4171@users.noreply.github.com>
2014-11-20 13:31:13 +0700

    Update ClientFile.java

--- ClientFile.java
+++ ClientFile.java
@@ -5,13 +5,6 @@
  */
 package clientfile;
 
-import java.io.DataInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 /**
  *
  * @author MAgus
@@ -24,31 +17,5 @@ public class ClientFile {
     public static void main(String[] args) {
         ServerForm form = new ServerForm();
         form.setVisible(true);
-        try {
-            // TODO code application logic here
-            File f = new File("F:/TEMP/PDM.jpg");
-            byte[] bytef = new byte[(int)f.length()];
-            DataInputStream diStream = new DataInputStream(new FileInputStream(f));
-            diStream.read(bytef);
-            /*Socket socket = new Socket("localhost", 9090);
-            ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
-            ObjectOutputStream ous = new ObjectOutputStream(socket.getOutputStream());
-            ObjectFile of = new ObjectFile();
-            of.setIsi(bytef);
-            of.setNama(f.getName());
-            of.setUser("AGUS");
-            of.setCommand("KIRIM");
-            
-            ous.writeObject(of);
-            ous.flush();
-            ous.reset();
-            
-            ois.close();
-            ois.close();
-            socket.close();*/
-        } catch (IOException ex) {
-            Logger.getLogger(ClientFile.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-    
+    }    
 }
